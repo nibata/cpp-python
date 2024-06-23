c94e1e6dcc8d325e3696ac5de0089557223af749
Nicolás Bacquet <nibata@gmail.com>
2024-06-23 17:37:00 -0400

    prueba 4

--- setup.py
+++ setup.py
@@ -1,4 +1,32 @@
 from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+import os
+
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=''):
+        super().__init__(name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+class CMakeBuild(build_ext):
+    def run(self):
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        cmake_args = [
+            '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+            '-DPYTHON_EXECUTABLE=' + self.get_python_executable()
+        ]
+
+        build_temp = os.path.abspath(self.build_temp)
+        os.makedirs(build_temp, exist_ok=True)
+        self.spawn(['cmake', ext.sourcedir] + cmake_args, cwd=build_temp)
+        self.spawn(['cmake', '--build', '.'], cwd=build_temp)
+
+    def get_python_executable(self):
+        import sys
+        return sys.executable
 
 setup(
     name='testpython',
@@ -6,6 +34,7 @@ setup(
     author='Nicolás Bacquet',
     author_email='nibata@gmail.com',
     description='An example package with PyBind11',
-    ext_modules=[Extension('testpython', ['src/bindings.cpp'])],
+    ext_modules=[CMakeExtension('testpython')],
+    cmdclass={'build_ext': CMakeBuild},
     zip_safe=False,
 )
